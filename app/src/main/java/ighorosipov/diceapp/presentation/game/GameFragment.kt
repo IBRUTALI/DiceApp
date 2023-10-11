ab5f1cbd1b60ac0fbd895221113983e5c1981159
Igor Osipov <ighor.osipov.95@mail.ru>
2023-10-11 14:08:47 +0300

    Create game fragment. Game interface was extended.

---
+++ app/src/main/java/ighorosipov/diceapp/presentation/game/GameFragment.kt
@@ -0,0 +1,31 @@
+package ighorosipov.diceapp.presentation.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ighorosipov.diceapp.databinding.FragmentGameBinding
+
+class GameFragment : Fragment() {
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGameBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/ighorosipov/diceapp/presentation/game/GameViewModel.kt
@@ -0,0 +1,4 @@
+package ighorosipov.diceapp.presentation.game
+
+class GameViewModel {
+}
